be8b4d9abab33bbc666f0d96ce23b0d02d90238d
Pawel Suchocki <pawel.suchocki212@gmail.com>
2018-04-29 18:56:11 +0200

    add comments in scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+// alert('test');
+// the function above has been commented and will not be executed
+
+/*
+  block comment
+  console.log('The commented will not be called');
+*/
+
+// linear comment
+
+// Below I have commented on a function call to disable it
+// console.log('The commented will not be called');
